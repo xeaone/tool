c454a91c34f90eb3af27d8b135a40f34115cc077
Alexander Elias <alex.steven.elias@gmail.com>
2022-12-04 15:35:17 -0800

    0.0.4

--- node/password/esm/mod.js
+++ node/password/esm/mod.js
@@ -6,31 +6,14 @@ const ITERATIONS = 999999;
 const randomBytes = function (size) {
     return dntShim.crypto.getRandomValues(new Uint8Array(size)).buffer;
 };
-const bufferToHex = function (buffer) {
-    const bytes = new Uint8Array(buffer);
-    const hex = new Array(bytes.length);
-    for (let i = 0, l = bytes.length; i < l; i++) {
-        hex[i] = bytes[i].toString(16).padStart(2, '0').slice(-2);
-    }
-    return hex.join('');
+const bufferToHex = function (data) {
+    return Array.from(new Uint8Array(data), x => x.toString(16).padStart(2, '0')).join('');
 };
-const stringToBuffer = function (string) {
-    const bytes = new Uint8Array(string.length);
-    for (let i = 0, l = string.length; i < l; i++) {
-        bytes[i] = string.charCodeAt(i);
-    }
-    return bytes.buffer;
+const stringToBuffer = function (data) {
+    return Uint8Array.from(data, x => x.charCodeAt(0)).buffer;
 };
-const hexToBuffer = function (hex) {
-    if (typeof hex !== 'string')
-        throw new TypeError('expected input to be a string');
-    if ((hex.length % 2) !== 0)
-        throw new RangeError('expected string to be an even number of characters');
-    const bytes = new Uint8Array(hex.length / 2);
-    for (let i = 0, l = hex.length; i < l; i += 2) {
-        bytes[i / 2] = parseInt(hex.substring(i, i + 2), 16);
-    }
-    return bytes.buffer;
+const hexToBuffer = function (data) {
+    return Uint8Array.from(data.match(/.{2}/gi) || [], x => parseInt(x, 16)).buffer;
 };
 const pbkdf2 = async function (password, salt, iterations, size, hash) {
     const key = await dntShim.dntGlobalThis.crypto.subtle.importKey('raw', password, {
--- post/mod.js
+++ post/mod.js
@@ -1,15 +1,17 @@
 /*
     license: MIT
-    version: 0.0.3
+    version: 0.0.4
     author: Alexander Elias
     repository: https://github.com/xeaone/tool
     jsdelivr: https://cdn.jsdelivr.net/gh/xeaone/tool@main/
 */
 export default class Post {
     beforePost;
+    duringPost;
     afterPost;
     constructor(options) {
         this.beforePost = options?.beforePost;
+        this.duringPost = options?.duringPost;
         this.afterPost = options?.afterPost;
     }
     async method(path, data) {
@@ -20,7 +22,19 @@ export default class Post {
         }
         catch { /**/ }
         const response = await globalThis.fetch(url.href, { method: 'POST', body: data });
-        data = await response.text();
+        if (!response.body) {
+            await this.afterPost?.(data, new URL(response.url), response.status);
+            return {};
+        }
+        data = '';
+        const decoder = new TextDecoder();
+        const reader = response.body.getReader();
+        let result = await reader.read();
+        while (!result.done) {
+            data += decoder.decode(result.value, { stream: true });
+            await this.duringPost?.(data, new URL(response.url), response.status);
+            result = await reader.read();
+        }
         try {
             data = JSON.parse(data);
         }
